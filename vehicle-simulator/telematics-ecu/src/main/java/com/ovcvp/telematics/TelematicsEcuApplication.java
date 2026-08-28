a3ce3123b922ec327070ffca92d0154970dc8f45
Pratik Mondal <pratikmondal6@hotmail.com>
2026-08-29 00:40:06 +0200

    Initialize vehicle connectivity validation platform

---
+++ vehicle-simulator/telematics-ecu/src/main/java/com/ovcvp/telematics/TelematicsEcuApplication.java
@@ -0,0 +1,13 @@
+package com.ovcvp.telematics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelematicsEcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelematicsEcuApplication.class, args);
+	}
+
+}
---
+++ vehicle-simulator/telematics-ecu/src/test/java/com/ovcvp/telematics/TelematicsEcuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovcvp.telematics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelematicsEcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
